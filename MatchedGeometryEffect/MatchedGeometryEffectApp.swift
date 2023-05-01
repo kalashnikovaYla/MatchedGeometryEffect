c863cbf3794095b3550a9332b79784bda1f2c8e6
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-05-01 20:56:06 +0500

    Initial Commit

---
+++ MatchedGeometryEffect/MatchedGeometryEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedGeometryEffectApp.swift
+//  MatchedGeometryEffect
+//
+//  Created by sss on 01.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct MatchedGeometryEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
